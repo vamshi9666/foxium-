958f152d625666df1e946dd261f8c3cca2d1d00e
vamshikn <vamshikn@upscalesoft.in>
2018-06-01 19:55:15 +0530

    nodemodules

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const {app, BrowserWindow} = require('electron')
-  const path = require('path')
-  const url = require('url')
+const path = require('path')
+const url = require('url')
   
   function createWindow () {
     // Create the browser window.
